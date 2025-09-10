45191ed6123487e28bed6b66a1490af0096945f8
cmitesh2503 <cmitesh@yahoo.com>
2025-09-10 11:33:18 +0530

    Change Temperature parameter 0.5 to 0.7 in llm_demo.py

--- LLM/llm_demo.py
+++ LLM/llm_demo.py
@@ -7,7 +7,7 @@ llm = HuggingFacePipeline.from_model_id(
     model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
         task="text-generation",
         pipeline_kwargs=dict(
-        temperature=0.5,
+        temperature=0.8,
         max_new_tokens=2048
         ) 
     )
